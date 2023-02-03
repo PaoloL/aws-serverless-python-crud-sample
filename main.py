590cfa7b677075c7bcf50c4f208eab23a914e898
Paolo Latella <maverick@Paolos-MacBook-Pro.local>
2023-02-03 21:45:38 +0100

    Added PUT function

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import src.GetBikes.webservice as get
+import src.PutBikes.webservice as put
+import src.PostBikes.webservice as post
 import json
 
 if __name__ == "__main__":
+    print("Simulate GET Bikes")
     get_event_file = open('events/get_bikes.json')
     event = json.load(get_event_file)
     context = []
     get.lambda_handler(event, context)
+
+    print("Simulate POST Bikes")
+    post_event_file = open('events/post_bikes.json')
+    event = json.load(post_event_file)
+    context = []
+    post.lambda_handler(event, context)
